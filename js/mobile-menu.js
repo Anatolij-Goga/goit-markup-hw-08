72506ef70473e2dfaa443893cbc25e2620ce22fd
Anatolij Goga <anzefal@gmail.com>
2022-10-08 17:34:20 +0300

    Правки по адаптивке

---
+++ js/mobile-menu.js
@@ -0,0 +1,27 @@
+(() => {
+  const openMenuBtn = document.querySelector('.open-menu');
+  const closeMenuBtn = document.querySelector('.close-menu');
+  const mobileMenu = document.querySelector('.menu');
+  const mobileContainer = document.querySelector('.header-container');
+
+  const toggleMenu = () => {
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-hidden');
+    mobileContainer.classList.toggle('is-hidden');
+
+    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+    mobileMenu.classList.remove('is-hidden');
+  });
+})();
